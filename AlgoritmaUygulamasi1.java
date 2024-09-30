4d0f297aa26830dd757830bf0e63fdd323ad3ef5
Muhsin DOLU <muhsindolu06@gmail.com>
2024-09-30 20:03:23 +0300

    Algoritma sorusu
    Kullanıcıdan bir sayı alınıp eğer beşten büyükse sayı faktoriyeli hesaplanır eğer beşten küçükse bir eksiği alınıp ikie bölünür ve sayı yuvarlanır en yakın değere bu algoritmayı uygulayan uygulama

---
+++ AlgoritmaUygulamasi1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class JavaApplication {
+	public static void main(String args[]) {
+		Scanner input=new Scanner(System.in);
+		//Kullanıcıdan sayı alınması
+		System.out.println("Kullanıcıdan sayı alınması: ");
+		int sayi=input.nextInt();
+		
+		int result=1;
+        //Eğer bu sayı 5'ten büyük ise sayının faktoriyeli hesaplanır
+		if(sayi>5) {
+			for(int i=1; i<=sayi;i++) {
+					result=result*i;
+			}
+		}
+        // Eğer sayı 5'ten küçük ise sayının bir eksiği alıarak 2 ye bölünür ve en yakın sayıya yuvarlanır
+        else {
+			result=(sayi-1)/2;
+		}
+        //Kullanıcının ekranına çıktı verilmesi
+		System.out.println("Cevap: "+result);
+	}
+}
