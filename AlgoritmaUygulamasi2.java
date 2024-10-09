0b36c4d1eafea38abddabd4f3dcc47447eb36eba
Muhsin DOLU <muhsindolu06@gmail.com>
2024-10-09 21:53:42 +0300

    Algoritma soru uygulaması
    Kullanıcının girdiği sayı 10'dan büyük ise faktöriyeli alınır eğer küçük ise 10 katı alınır bunu ekranda gösteren uygulama

---
+++ AlgoritmaUygulamasi2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class JavaApplication {
+	public static void main(String args[]) {
+		Scanner input=new Scanner(System.in);
+		//Kullanıcıdan sayı alınması
+		System.out.println("Sayı giriniz: ");
+		int sayi=input.nextInt();
+		float result=1;
+		// Sayı 10'dan büyük ise faktoriyeli hesaplanır
+		if(sayi>=10) {
+			for (int i = 2; i <= sayi; i++) {
+				result*=i;
+			}
+		}
+		// Sayı 10'dan küçük ise 10 katı alınır sayının
+		else {
+			result=sayi*10;
+		}
+		// Sonucun ekranda gösterilmesi
+		System.out.println("CEVAP: "+result);
+	}
+}
