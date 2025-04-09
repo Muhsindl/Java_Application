5d02c1cce3ea0abc3281728f1ee4c3c21b880ce5
Muhsin DOLU <muhsindolu06@gmail.com>
2025-04-09 08:25:06 +0300

    Matris random notlar ile  maximum ders notlarını gösteren uygulama
    Kullanıcı tarafından girilen öğrenci ve ders sayısına ait random oluşturulan notlar ile her derse ait maximum notu gösteren uygulama

---
+++ MatrisRandomDersNotlarınaAitMaximumNotlarıBulmaUygulamasi.java
@@ -0,0 +1,58 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Kullanıcıdan ders sayısının alınması
+        System.out.println("Ders sayısı gir: ");
+        int col = input.nextInt();
+
+        // Kullanıcıdan öğrenci sayısının alınması
+        System.out.println("Öğrenci sayısı gir: ");
+        int row = input.nextInt();
+
+        int[][] matris = new int[row][col];
+        Random r = new Random();
+
+        // Her öğrencinin aldığı derse ait rastgele not atanması
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                matris[i][j] = r.nextInt(101); // 0-100 arasında rastgele not
+            }
+        }
+
+        // Notların ekranda gösterilmesi (Sütunlar ders, satırlar öğrenciyi temsil eder)
+        System.out.println("Öğrencilerin ders notları:");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(matris[i][j] + "\t");
+            }
+            System.out.println();
+        }
+
+        // Maksimum notları bulma
+        int[] maxNot = new int[col]; // Ders sayısına göre boyutlandırılan dizi
+
+        // Başlangıçta her dersin ilk notunu maksimum olarak kabul ediyoruz
+        for (int j = 0; j < col; j++) {
+            maxNot[j] = matris[0][j];
+        }
+
+        // Her dersin maksimum notunu bulma
+        for (int j = 0; j < col; j++) {
+            for (int i = 1; i < row; i++) {
+                if (matris[i][j] > maxNot[j]) {
+                    maxNot[j] = matris[i][j];
+                }
+            }
+        }
+
+        // Her ders için maksimum notları ekrana yazdırma
+        System.out.println("Her ders için maksimum notlar:");
+        System.out.println(Arrays.toString(maxNot));
+    }
+}
