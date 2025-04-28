a8ce9601f64c9fc2396a4e297a85ad374fe102c0
Muhsin DOLU <muhsindolu06@gmail.com>
2025-04-28 08:32:53 +0300

    Matris köşegen sıfır yapan metot uygulaması
    Kullanıcı tarafından girilen eleman sayısına ve elemanlara göre oluşturulan kare matriste  köşegeni sıfır yapan metot uygulaması

---
+++ MatrisKosegeniSifirYapanUygulama.java
@@ -0,0 +1,55 @@
+//Matris köşegenini 0 sayısı yapma
+
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        // Kullanıcıdan eleman sayısının alınması
+        System.out.println("Eleman sayısı: ");
+        int eleman = input.nextInt();
+        
+        // Girilen eleman sayısına göre kare matris oluşturulması
+        int[][] matris = new int[eleman][eleman];
+        
+        //Kullanıcıdan elemanların alınması
+        for (int i = 0; i < eleman; i++) {
+            for (int j = 0; j < eleman; j++) {
+                System.out.println("("+(i)+","+(j)+") Elemanı giriniz: ");
+                matris[i][j] = input.nextInt(); 
+            }
+        }
+        // Matrisin ekrana yazdırılması
+        System.out.println("-----------------Matris-----------------");
+        for (int i = 0; i < eleman; i++) {
+            for (int j = 0; j < eleman; j++) {
+                System.out.print(matris[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.println("-***************************************-");
+        // Metota matrisin gönderilmesi
+        func(matris);
+    }
+    // Matrisin köşegenlerini sıfır yapan  metot
+    static void func(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (i + j == matrix.length - 1 || i==j) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        // Güncellenmiş matrisin ekrana yazdırılması
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
