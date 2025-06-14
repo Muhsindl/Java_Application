8c98e6dbb640bd0bb237efacd4045af0066008d6
Muhsin DOLU <94922888+Muhsindl@users.noreply.github.com>
2025-06-14 22:45:41 +0300

    CumleBirlestirmeVeEklemeUygulamasi.java

--- CumleBirlestirmeVeEklemeUygulamasi/src/main/java/CumleBirlestirmeVeEklemeUygulamasi.java
+++ CumleBirlestirmeVeEklemeUygulamasi/src/main/java/CumleBirlestirmeVeEklemeUygulamasi.java
@@ -5,20 +5,20 @@ public class CumleBirlestirmeVeEklemeUygulamasi {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        // Kullanıcıdan ilk cümlenin alınması
         System.out.println("İlk cümleyi giriniz: ");
         String ilkCumle = input.nextLine();
-
+        // Kullanıcıdan son cümlenin alınması
         System.out.println("Son cümleyi giriniz: ");
         String sonCumle = input.nextLine();
-
+        // Kullanıcıdan araya eklenecek cümlenin alınması
         System.out.println("Araya eklenecek cümleyi giriniz: ");
         String ortaCumle = input.nextLine();
-
+        // cumleBirlestir() metoduna girilen cümlelerin gönderilmesi ve cümle birleştirilmesinin yapılması 
         cumleBirlestir(ilkCumle, sonCumle, ortaCumle);
 
     }
-
+    // Cümle birleştiren metot
     static void cumleBirlestir(String ilk, String son, String orta) {
         StringBuffer bfr = new StringBuffer(ilk);
         bfr.append(son);
