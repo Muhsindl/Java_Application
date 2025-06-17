38b0fe32d0e4d9e85ab2ea7711251ab836bed296
Muhsin DOLU <94922888+Muhsindl@users.noreply.github.com>
2025-06-17 10:18:06 +0300

    CumleKarakterSilmeUygulamasi.java

--- CumleAralikKarakterSilmeUygulamasi/src/main/java/CumleKarakterSilmeUygulamasi.java
+++ CumleAralikKarakterSilmeUygulamasi/src/main/java/CumleKarakterSilmeUygulamasi.java
@@ -5,20 +5,20 @@ public class CumleKarakterSilmeUygulamasi {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        // Kullanıcıdan cümlenin alınması
         System.out.println("Cümle giriniz: ");
         String text = input.nextLine();
         StringBuffer str = new StringBuffer(text);
-
+        // Kullanıcıdan alt sınır değerinin alınması
         System.out.println("Alt Aralık giriniz: ");
         int altLimit = input.nextInt();
-
+        // Kullanıcıdan üst sınır değerinin alınması
         System.out.println("Üst Aralık giriniz: ");
         int ustLimit = input.nextInt();
-
+        // Kullanıcı tarafından girilen değerlerin metota gönderilmesi
         karakterSil(str, altLimit, ustLimit);
     }
-
+    // Alt ve üst sınır arasında kalan karakterlerin silinmesi
     static void karakterSil(StringBuffer txt, int alt, int ust) {
         txt.delete(alt, ust);
         System.out.println(txt);
