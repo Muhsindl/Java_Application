349724595cc705bf7041bde86d496abe8bb03f0d
Muhsin DOLU <94922888+Muhsindl@users.noreply.github.com>
2025-06-19 07:51:00 +0300

    CumleBoslukKaldirmaUygulamasi.java

--- CumleBoslukKaldirmaUygulamasi/src/main/java/CumleBoslukKaldirmaUygulamasi.java
+++ CumleBoslukKaldirmaUygulamasi/src/main/java/CumleBoslukKaldirmaUygulamasi.java
@@ -5,13 +5,15 @@ public class CumleBoslukKaldirmaUygulamasi {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        // Kullanıcı tarafından cümlenin girilmesi
         System.out.println("Cümle giriniz: ");
         String boslukCumle = input.nextLine();
-
+        // Girilen cümlenin metoda gönderilmesi
         cumleBoslukKaldir(boslukCumle);
     }
 
+    // Cümleden boşlukları kaldıran metot 
+    
     static void cumleBoslukKaldir(String text) {
         StringBuffer bfr = new StringBuffer(text);
         for (int i = 0; i < bfr.length(); i++) {
