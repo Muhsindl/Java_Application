d54bdcf02e87ea0c14c6a2537897d3ac3eed9f13
Muhsin DOLU <94922888+Muhsindl@users.noreply.github.com>
2025-06-20 07:47:03 +0300

    CumleEpostaOneriUygulamasi.java

--- CumleEpostaOneriUygulamasi/src/main/java/CumleEpostaOneriUygulamasi.java
+++ CumleEpostaOneriUygulamasi/src/main/java/CumleEpostaOneriUygulamasi.java
@@ -5,18 +5,18 @@ public class CumleEpostaOneriUygulamasi {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        // Kullanıcı tarafından isim girilmesi
         System.out.println("İsim giriniz: ");
         String name = input.nextLine();
-
+        // Kullanıcı tarafından soyisim girilmesi
         System.out.println("Soyisim giriniz: ");
         String surname = input.nextLine();
-
+        // Kullanıcı tarafından girilen isim ve soyisimleri küçük harfe çevirip metota gönderilmesi    
         name = name.toLowerCase();
         surname = surname.toLowerCase();
         ePostaDonusturucu(name, surname);
     }
-
+    // E-posta öneri metodu
     static void ePostaDonusturucu(String nm, String snm) {
         StringBuffer bfr = new StringBuffer();
         bfr.append(snm);
