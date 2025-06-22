7d9fd0065bb5e777e8f12ce865eeeee70ad733e6
Muhsin DOLU <94922888+Muhsindl@users.noreply.github.com>
2025-06-22 14:41:03 +0300

    CumleKelimeDegistirmeUygulamasi.java

--- CumleKelimeDegistirmeUygulamasi/src/main/java/CumleKelimeDegistirmeUygulamasi.java
+++ CumleKelimeDegistirmeUygulamasi/src/main/java/CumleKelimeDegistirmeUygulamasi.java
@@ -5,21 +5,26 @@ public class CumleKelimeDegistirmeUygulamasi {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
+        // Kullanıcıdan cümlenin alınması
         System.out.println("Cümle giriniz: ");
         String text = input.nextLine();
-
+        
+        // Kullanıcıdan değiştirilecek kelimenin alınması
         System.out.println("Eski kelime hangisi: ");
         String oldText = input.nextLine();
-
+        
+        // Kullanıcıdan değiştirilmesi istenilen kelimenin alınması
         System.out.println("Değiştirilecek kelime: ");
         String newText = input.nextLine();
-
+        
+        // Eski ve yeni cümlenin ekranda gösterilmesi
         System.out.println("Eski Cümle Yapısı; \n" + text);
         System.out.println("----------------------------");
-        System.out.println("Yeni Cümle Yapısı; \n" + text);
+        System.out.println("Yeni Cümle Yapısı; \n");
         kelimeDegistirme(text, oldText, newText);
     }
-
+    // Kelime değiştirme metot uygulaması
     static void kelimeDegistirme(String text, String oldText, String newText) {
         StringBuffer bfr = new StringBuffer(text);
         int eskiKelimeIndis = bfr.indexOf(oldText);
