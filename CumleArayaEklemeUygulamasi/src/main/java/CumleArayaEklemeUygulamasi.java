d5e6a7a153749063a577e4b6b6e774d941e69a3f
Muhsin DOLU <94922888+Muhsindl@users.noreply.github.com>
2025-06-25 07:52:56 +0300

    CumleArayaEklemeUygulamasi.java

--- CumleArayaEklemeUygulamasi/src/main/java/CumleArayaEklemeUygulamasi.java
+++ CumleArayaEklemeUygulamasi/src/main/java/CumleArayaEklemeUygulamasi.java
@@ -5,16 +5,17 @@ public class CumleArayaEklemeUygulamasi {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        // Kullanıcıdan cümlenin alınması
         System.out.println("Cümle giriniz: ");
         String txt = input.nextLine();
-
+        
+        // Kullanıcıdan araya eklenecek cümlenin alınması
         System.out.println("Araya eklenecek cümle giriniz: ");
         String betweenTxt = input.nextLine();
-
+        // Kullanıcının tarafından girilen cümle ve araya eklenecek cümlenin arayaCumleEkleme() metoduna gönderilmesi
         arayaCumleEkleme(txt, betweenTxt);
     }
-
+    // Cümlenin tam ortasına yeni cümle ekleyen metot 
     static void arayaCumleEkleme(String text, String btwnTxt) {
         StringBuffer bfrTxt = new StringBuffer(text);
         bfrTxt.insert(bfrTxt.length() / 2, btwnTxt);
