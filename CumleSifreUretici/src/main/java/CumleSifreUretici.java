0e1a249b39649cc907fcf4f212ed17a2aa384128
Muhsin DOLU <94922888+Muhsindl@users.noreply.github.com>
2025-06-26 07:42:02 +0300

    CumleSifreUretici.java

--- CumleSifreUretici/src/main/java/CumleSifreUretici.java
+++ CumleSifreUretici/src/main/java/CumleSifreUretici.java
@@ -5,13 +5,13 @@ public class CumleSifreUretici {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        //Kullanıcıdan cümlenin alınması
         System.out.println("Cümle giriniz: ");
         String text = input.nextLine();
-
+        // Kullanıcıdan alınan cümlenin sifreUretici() metoduna gönderilmesi
         sifreUretici(text);
     }
-
+    // sifreUretici() metodu ile cümleye özelleştirilmiş şifre uygulanması
     static void sifreUretici(String text) {
         StringBuffer bfr = new StringBuffer(text);
         bfr.append("123");
