619f44906089d9d4a2dea19d25736915fcad5e0a
Muhsin DOLU <94922888+Muhsindl@users.noreply.github.com>
2025-06-27 07:51:11 +0300

    CumleTarihAtamaUygulamasi.java

--- CumleTarihAtamaUygulamasi/src/main/java/CumleTarihAtamaUygulamasi.java
+++ CumleTarihAtamaUygulamasi/src/main/java/CumleTarihAtamaUygulamasi.java
@@ -5,17 +5,20 @@ public class CumleTarihAtamaUygulamasi {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        // Kullanıcı tarafından cümle girilmesi
         System.out.println("Cümle giriniz: ");
         String txt = input.nextLine();
-
+        
+        // Kullanıcıdan tarih bilgisinin alınması
         System.out.println("Tarih giriniz: ");
         String trh = input.nextLine();
-
+       
+        // tarihAta() fonksiyonuna cümle ve tarih bilgilerinin gönderilmesi
         tarihAta(txt, trh);
 
     }
-
+    
+    // Tarih ile cümleyi birleştirip ekrana yazdıran uygulama
     static void tarihAta(String txt, String tarih) {
         StringBuffer bfrCumle = new StringBuffer(txt);
         bfrCumle.insert(0, "Tarihli Cümle: ");
