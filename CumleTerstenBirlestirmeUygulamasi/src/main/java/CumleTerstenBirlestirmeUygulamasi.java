8e730b9f3150591a69590cf88e8014e94d236935
Muhsin DOLU <94922888+Muhsindl@users.noreply.github.com>
2025-06-28 12:51:10 +0300

    CumleTerstenBirlestirmeUygulamasi.java

--- CumleTerstenBirlestirmeUygulamasi/src/main/java/CumleTerstenBirlestirmeUygulamasi.java
+++ CumleTerstenBirlestirmeUygulamasi/src/main/java/CumleTerstenBirlestirmeUygulamasi.java
@@ -5,13 +5,13 @@ public class CumleTerstenBirlestirmeUygulamasi {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-
+        // Kullanıcıdan tersi ile birleştirilecek cümle alınması
         System.out.println("Tersi ile birleştirecek cümleyi giriniz: ");
         String txt = input.nextLine();
-
+        // terstenBirlestir() metoduna kullanıcıdan alınan cümlenin gönderilmesi
         terstenBirlestir(txt);
     }
-
+    // terstenBirlestir() metodu ile cümleyi tersi ile birleştiren metot
     static void terstenBirlestir(String txt) {
         StringBuffer text = new StringBuffer(txt);
 
