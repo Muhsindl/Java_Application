d810d1f04fb400a71644c1c0472e450ef2c689a0
Muhsin DOLU <94922888+Muhsindl@users.noreply.github.com>
2025-06-29 12:59:28 +0300

    TerstenCumleYazirmaMetodu.java

--- CumleTerstenYazdirmaUygulamasi/src/main/java/TerstenCumleYazirmaMetodu.java
+++ CumleTerstenYazdirmaUygulamasi/src/main/java/TerstenCumleYazirmaMetodu.java
@@ -5,10 +5,14 @@ public class TerstenCumleYazirmaMetodu {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // Kullanıcıdan kelime alınması
         System.out.print("Seçilen kelimeyi giriniz: ");
         String text = input.nextLine();
+        
+        // Kullanıcıdan alınan kelimeyi tersYazdirma() metoduna gönderir
         tersYazdirma(text);
     }
+    // tersYazdirma() kelimeyi tersten yazdıran metot
     static void tersYazdirma(String txt){
         for (int i = txt.length()-1; i >= 0; i--) {
             System.out.print(txt.charAt(i));
